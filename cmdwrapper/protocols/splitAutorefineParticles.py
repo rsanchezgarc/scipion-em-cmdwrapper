7e4700426c8b70bfcd3c3034020970d44a95290e
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2023-08-28 19:41:01 +0100

    bug fixed split

--- cmdwrapper/protocols/splitAutorefineParticles.py
+++ cmdwrapper/protocols/splitAutorefineParticles.py
@@ -24,12 +24,17 @@
 # *  e-mail address 'you@yourinstitution.email'
 # *
 # **************************************************************************
+import glob
 import os
+import random
+import starfile
+from pwem import ALIGN_PROJ
 
 from pwem.protocols import ProtProcessParticles
-from pyworkflow.protocol import PointerParam, IntParam
+from pyworkflow.protocol import PointerParam, IntParam, BooleanParam
 from pyworkflow.utils import makePath
-from relion.convert import writeSetOfParticles, readSetOfParticles
+from relion.convert import readSetOfParticles
+from starstack.particlesStar import ParticlesStarSet
 
 
 class ProtRelionAutorefSplitData(ProtProcessParticles):
@@ -38,52 +43,69 @@ class ProtRelionAutorefSplitData(ProtProcessParticles):
 
     def _defineParams(self, form):
         form.addSection(label='Input')
-        # Directly add a pointer parameter to select the previous autorefine run
         form.addParam('inputAutoRefineRun', PointerParam,
-                      pointerClass='ProtRelionAutoRefine',
+                      pointerClass='ProtRelionRefine3D',
                       label='Select autorefine run',
                       help='Select a previous autorefine run to split the particle stacks.')
-
         form.addParam('numberOfSubsets', IntParam, default=2,
                       label="Number of subsets",
                       help="Number of subsets to split the particles into.")
+        form.addParam('randomize', BooleanParam, default=False,
+                      label="Randomize particles?",
+                      help="If set to True, particles will be randomly assigned to subsets.")
 
     def _insertAllSteps(self):
-
         self._insertFunctionStep('splitRunStep')
 
-    def splitRunStep(self):
-        # Get the particles directly from the autorefine run input
-        inputParticles = self.inputAutoRefineRun.get().outputParticles
-
-        nSubsets = self.numberOfSubsets.get()
-        nParticles = inputParticles.getSize()
-        particlesPerSubset = nParticles // nSubsets
-
-        subsetLists = [[] for _ in range(nSubsets)]
-
-        for idx, particle in enumerate(inputParticles):
-            subsetIndex = idx // particlesPerSubset
-            if subsetIndex >= nSubsets:
-                subsetIndex = nSubsets - 1  # push remaining particles into the last subset
-            subsetLists[subsetIndex].append(particle)
-
-        for subsetIndex, subset in enumerate(subsetLists):
-            outputSet = self._createSetOfParticles()
-            outputSet.copyInfo(inputParticles)
-            outputSetPath = self._getExtraPath(f"subset_{subsetIndex}")
-            makePath(outputSetPath)
-            subsetStar = os.path.join(outputSetPath, "particles.star")
-
-            # Write the particles to a new star file
-            writeSetOfParticles(subset, subsetStar, outputDir=outputSetPath)
-
-            # This assumes there's a function in relion.convert like readSetOfParticles
-            # to populate the Scipion object from a star file
-            readSetOfParticles(subsetStar, outputSet)
-
-            self._defineOutputs(**{f"outputSubset_{subsetIndex}": outputSet})
-            self._defineTransformRelation(inputParticles, outputSet)
 
     def _methods(self):
-        return ["Particles were split into %d subsets." % self.numberOfSubsets.get()]
+        return [f"Particles were split into {self.numberOfSubsets.get()} subsets."]
+
+    def splitRunStep(self):
+        inputParticles = self.inputAutoRefineRun.get().outputParticles
+        extra_path = self.inputAutoRefineRun.get()._getExtraPath()
+        list_of_files = glob.glob(os.path.join(extra_path, '*_data.star'))
+        latest_file = max(list_of_files, key=os.path.getctime)
+
+        starData = starfile.read(latest_file)
+
+        psset = ParticlesStarSet(latest_file)
+
+        particles_data = starData['particles']
+        n_parts = len(particles_data)
+        idxs = list(range(n_parts))
+        if self.randomize.get():
+            print("Shuffling idxs")
+            random.shuffle(idxs)
+
+        nSubsets = self.numberOfSubsets.get()
+        nParticles = len(particles_data)
+        particlesPerSubset = nParticles // nSubsets
+
+        for subsetIndex in range(nSubsets):
+            start_idx = subsetIndex * particlesPerSubset
+            end_idx = start_idx + particlesPerSubset if subsetIndex < nSubsets - 1 else nParticles
+            subsetStar = self._getExtraPath(f"subset_{subsetIndex}", f"particles_{subsetIndex}.star")
+            subsetMrcs = self._getExtraPath(f"subset_{subsetIndex}", f"particles_{subsetIndex}.mrcs")
+            print("Writing here : ", os.path.split(subsetStar)[0])
+            os.makedirs(os.path.split(subsetStar)[0], exist_ok=True)
+            print(start_idx, end_idx)
+            psset_subset = psset.createSubset(start_idx, end_idx)
+            scipion_starfile = self._getTmpPath(f"subset_{subsetIndex}_particles.star")
+            psset_subset.save(scipion_starfile,
+                               stackFname=None, overwrite=True, basenameOnlyForNewStar=False)
+            psset_subset.save(subsetStar, stackFname=subsetMrcs, overwrite=True, basenameOnlyForNewStar=True)
+
+            #TODO: This part is not working, the N outputs of the protocol are the same.
+            # outputSet = self._createSetOfParticles()
+            # outputSet.copyInfo(inputParticles)
+            # readSetOfParticles(scipion_starfile, outputSet, alignType=ALIGN_PROJ)
+            # outputs = {}
+            # outputs[f"outputSubset_{subsetIndex}"] = outputSet
+            # self._defineOutputs(**outputs)
+            # print(outputSet)
+        # for n,o in outputs.items():
+        #     print(n,o)
+        #     self._defineOutputs(n=o)
+        #     self._store(o)
+            # self._defineTransformRelation(inputParticles, o)
